4114e6a4aafa1ce117e1878320a5d9fa6b023e82
FrontiMaxim <FrontiMax@yandex.ru>
2021-12-05 16:03:36 +0400

    create REST API

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const articles = [{ title: 'Example'}];
+
+app.set('port', process.env.PORT || 8080);
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello!');
 });
 
-app.listen(port, () => {
-    console.log(`Express web app available at localhost: ${port} `);
+app.get('/articles', (req, res, next) => {
+    res.send(articles);
 });
+
+app.post('/articles', (req, res, next) => {
+    res.send('OK');
+});
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Fetching:', id);
+    res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Deleting:', id);
+    delete articles[id];
+    res.send({ message: 'Deleted '});
+});
+
+app.listen(app.get('port'), () => {
+    console.log('App started on port', app.get('port'));
+});
+
+module.exports = app;
