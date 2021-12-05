516e22b7143a203c3b491e3711d38e834a234661
FrontiMaxim <FrontiMax@yandex.ru>
2021-12-05 16:46:30 +0400

    added body-parser and changed post request for creating articles

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const articles = [{ title: 'Example'}];
 
 app.set('port', process.env.PORT || 8080);
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello!');
@@ -14,7 +18,9 @@ app.get('/articles', (req, res, next) => {
 });
 
 app.post('/articles', (req, res, next) => {
-    res.send('OK');
+    const article = { title : req.body.title }; 
+    articles.push(article ); 
+    res.send(article);
 });
 
 app.get('/articles/:id', (req, res, next) => {
